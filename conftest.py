c7157dc885e553b7c963ee8446506ec8f0ea8518
Margo <margarita.pristromskaya@gmail.com>
2023-07-31 18:05:03 +0200

    fixed settings

--- conftest.py
+++ conftest.py
@@ -4,8 +4,8 @@ from selene.support.shared import browser
 
 @pytest.fixture
 def browser_open():
-    browser.config.window_width = 640
-    browser.config.window_height = 480
+    browser.config.window_width = 1980
+    browser.config.window_height = 1200
     browser.config.browser_name = 'chrome'
     browser.open('https://google.com')
 
